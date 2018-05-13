30a718c20eacda27dc21c6374d4abbc90db08e22
BGM-liverpool <39246512+BGM-liverpool@users.noreply.github.com>
2018-05-13 21:04:17 +0100

    Add files via upload

---
+++ cmo_ode.m
@@ -0,0 +1,19 @@
+function [dydt] = cmo_ode(~,y,param,dopuse,nulim)
+%
+ rate = cmo_prog.cmo_rate(y,param,dopuse,nulim);
+ DIN  = y(:,1);
+ DIP  = y(:,2);
+ PhyC = y(:,3);
+ PhyN = y(:,4);
+ PhyP = y(:,5);
+ Chl  = y(:,6);
+ DOP  = y(:,7);
+%
+dydt = zeros(size(y));
+dydt(:,1) = - rate.VN.*PhyC + rate.mortN;    %unit: umolN/L
+dydt(:,2) = - rate.VDIP.*PhyC + rate.mortP;    %unit: umolP/L
+dydt(:,3) =   rate.VC.*PhyC - rate.mortC-rate.lys.*PhyC;    %unit: umolC/L
+dydt(:,4) =   rate.VN.*PhyC-rate.mortN - param.Q0N.*dydt(:,3);   %unit: umolN/L
+dydt(:,5) =   rate.VP.*PhyC-rate.mortP - param.Q0P.*dydt(:,3)-rate.lys./2.*PhyP;   %unit: umolP/L
+dydt(:,6) =   rate.Pchl - rate.mortChl; %unit: ugChl/L
+dydt(:,7) =  -rate.VDOP.*PhyC+rate.lys./2.*PhyP; %unit: umolP/L
